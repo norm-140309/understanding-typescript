82c3daf25b94e69c45eb6dce63e540e5125a55a8
Norm Glomski <nglomski@gmail.com>
2017-10-26 11:46:23 -0500

    Finished static properties and methods, as well as Abstract classes lessons.

--- app.js
+++ app.js
@@ -41,3 +41,80 @@ var Norm = (function (_super) {
 }(Person));
 var person2 = new Norm('normando2');
 console.log('Person2: ', person2);
+//Getters and Setters
+var Plant = (function () {
+    function Plant() {
+        this._species = "Default";
+    }
+    Object.defineProperty(Plant.prototype, "species", {
+        get: function () {
+            return this._species;
+        },
+        set: function (value) {
+            if (value.length > 3) {
+                this._species = value;
+            }
+            else {
+                this._species = "Default";
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Plant;
+}());
+var plant = new Plant();
+plant.species = 'Rose';
+console.log('plant:', plant.species);
+//Static Properties and Methods
+var Helpers = (function () {
+    function Helpers() {
+    }
+    Helpers.calcCircumference = function (diameter) {
+        return this.PI * diameter;
+    };
+    return Helpers;
+}());
+Helpers.PI = 3.14;
+console.log('PI:', 2 * Helpers.PI);
+console.log('Circumference:', Helpers.calcCircumference(8));
+//Abstract Classes
+var Project = (function () {
+    function Project() {
+        this.projectName = "Default";
+    }
+    Project.prototype.calcBudget = function () {
+        return this.budget * 2;
+    };
+    return Project;
+}());
+var ITProject = (function (_super) {
+    __extends(ITProject, _super);
+    function ITProject() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ITProject.prototype.changeName = function (name) {
+        this.projectName = name;
+    };
+    return ITProject;
+}(Project));
+var newProject = new ITProject();
+console.log('newProject:', newProject);
+newProject.changeName("Super IT Project");
+console.log('newProject:', newProject);
+// private constructors
+var OnlyOne = (function () {
+    function OnlyOne(name) {
+        this.name = name;
+    }
+    OnlyOne.getInstance = function () {
+        if (!OnlyOne.instance) {
+            OnlyOne.instance = new OnlyOne('The Only One');
+        }
+        return OnlyOne.instance;
+    };
+    return OnlyOne;
+}());
+// let wrong = new OnlyOne('The Only One');
+var right = OnlyOne.getInstance();
+console.log('Right name:', right.name);
--- app.ts
+++ app.ts
@@ -32,3 +32,69 @@ class Norm extends Person {
 }
 const person2 = new Norm('normando2');
 console.log('Person2: ', person2);
+
+//Getters and Setters
+class Plant {
+    private _species: string = "Default";
+    get species() {
+        return this._species;
+    }
+    set species(value: string) {
+        if(value.length > 3) {
+            this._species = value;
+        } else {
+            this._species = "Default";
+        }
+    } 
+}
+let plant = new Plant();
+plant.species = 'Rose';
+console.log('plant:', plant.species);
+
+//Static Properties and Methods
+class Helpers {
+    static PI: number = 3.14;
+    static calcCircumference(diameter: number): number {
+        return this.PI * diameter;
+    }
+}
+console.log('PI:', 2 * Helpers.PI);
+console.log('Circumference:', Helpers.calcCircumference(8));
+
+//Abstract Classes
+abstract class Project {
+    projectName: string = "Default";
+    budget: number;
+
+    abstract changeName(name: string): void;
+
+    calcBudget() {
+        return this.budget * 2;
+    }
+}
+
+class ITProject extends Project {
+    changeName(name: string): void {
+        this.projectName = name;
+    }
+}
+
+let newProject = new ITProject();
+console.log('newProject:', newProject);
+newProject.changeName("Super IT Project");
+console.log('newProject:', newProject);
+
+// private constructors
+class OnlyOne {
+    private static instance: OnlyOne;
+    private constructor(public name: string) {}
+    static getInstance() {
+        if(!OnlyOne.instance) {
+            OnlyOne.instance = new OnlyOne('The Only One');
+        }
+        return OnlyOne.instance;
+    }
+}
+// let wrong = new OnlyOne('The Only One');
+let right = OnlyOne.getInstance();
+console.log('Right name:', right.name);
